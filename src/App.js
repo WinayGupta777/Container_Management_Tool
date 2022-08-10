21c805d73cd2531d999c322d0a825fb2bac94efc
WinayGupta777 <vinay.gupta8101@gmail.com>
2022-08-10 13:48:46 +0530

    Drawer Initialized;

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import React from 'react'
 import AppBarComp from './components/AppBar';
+import DrawerMain from './components/DrawerMain';
 const App = () => {
   return (
     <div>
       <AppBarComp />
+      <DrawerMain />
     </div>
   )
 }
---
+++ src/components/DrawerMain.js
@@ -0,0 +1,27 @@
+import { Divider, Drawer, Toolbar } from '@mui/material';
+import React from 'react'
+
+const DrawerMain = () => {
+  return (
+    <div>
+        <Drawer
+            anchor="left" variant="permanent" elevation={20}
+            sx={{
+                opacity: 1,
+                width: 300,
+                flexShrink: 0,
+                boxSizing: 'border-box',
+                '& .MuiDrawer-paper': {
+                    width: 330,
+                },
+            }}
+        >
+            <Toolbar sx={{height: "5rem"}} />
+            Hello
+            <Divider />
+        </Drawer>
+    </div>
+  )
+}
+
+export default DrawerMain;
