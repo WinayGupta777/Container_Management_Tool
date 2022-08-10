310819afc69a6487d33b0566cef79d56872e5746
WinayGupta777 <vinay.gupta8101@gmail.com>
2022-08-10 14:50:04 +0530

    Created assets dir for custom icons; System module added in DrawerListing;

--- src/components/DrawerListing.js
+++ src/components/DrawerListing.js
@@ -1,18 +1,33 @@
 import React from 'react'
-import { Box, List, ListItemButton } from '@mui/material';
-
+import { Box, Collapse, List, ListItemButton, ListItemIcon, ListItemText, SvgIcon } from '@mui/material';
+import { ArrowDropDown } from '@mui/icons-material';
+import DesktopWindowsIcon from "@mui/icons-material/DesktopWindows"
+import { ReactComponent as Termi } from "../assets/TerminalIcon.svg";
 const DrawerListing = () => {
-  return (
-    <div>
-        <Box>
-            <List>
-                <ListItemButton>
-                    Docker
-                </ListItemButton>
-            </List>
-        </Box>
-    </div>
-  )
+    return (
+        <div>
+            <Box>
+                <List>
+                    <ListItemButton>
+                        <ListItemIcon>
+                            {/* System */}
+                            <DesktopWindowsIcon />
+                        </ListItemIcon>
+                        <ListItemText primary="System" />
+                        <ArrowDropDown />
+                    </ListItemButton>
+                    <Collapse in={true} timeout="auto" unmountOnExit>
+                        <ListItemButton sx={{ pl: 5 }}>
+                            <ListItemIcon>
+                                <SvgIcon component={Termi} inheritViewBox></SvgIcon>
+                            </ListItemIcon>
+                            <ListItemText primary="Terminal" />
+                        </ListItemButton>
+                    </Collapse>
+                </List>
+            </Box>
+        </div>
+    )
 }
 
 export default DrawerListing;
