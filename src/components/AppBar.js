0f4ba1d40a786bee4c5da83dfc397aeefd1f3fdc
WinayGupta777 <vinay.gupta8101@gmail.com>
2022-08-10 15:08:11 +0530

    Theme icon added;

--- src/components/AppBar.js
+++ src/components/AppBar.js
@@ -1,8 +1,16 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { AppBar, IconButton, Toolbar, Tooltip, Typography } from '@mui/material';
 import GitHubIcon from '@mui/icons-material/GitHub';
+import BrightnessHighIcon from '@mui/icons-material/BrightnessHigh';
+import BrightnessLowIcon from '@mui/icons-material/BrightnessLow';
+
 
 const AppBarComp = () => {
+    const [globalThemeMode, setGlobalThemeMode] = useState(false);
+
+    const changeTheme=()=>{
+        setGlobalThemeMode(!globalThemeMode);
+    }
     return (
         <div>
             <AppBar
@@ -15,6 +23,13 @@ const AppBarComp = () => {
                     <Tooltip title="Source code">
                         <IconButton size='large' color='inherit' sx={{ mr: 2 }}>
                             <GitHubIcon />
+                        </IconButton>
+                    </Tooltip>
+                    <Tooltip title="Change theme">
+                        <IconButton size='large' color='inherit' sx={{ mr: 2 }} onClick={()=>changeTheme()}>
+                            { globalThemeMode ? 
+                            <BrightnessHighIcon />
+                            : <BrightnessLowIcon />}
                         </IconButton>
                     </Tooltip>
                 </Toolbar>
