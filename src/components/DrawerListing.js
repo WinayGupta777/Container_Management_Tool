4b601800c7d11ca99bde735ca000e506e034d02a
WinayGupta777 <vinay.gupta8101@gmail.com>
2022-08-10 16:32:01 +0530

    ListItemCollapse modified;

--- src/components/DrawerListing.js
+++ src/components/DrawerListing.js
@@ -1,33 +1,50 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Box, Collapse, List, ListItemButton, ListItemIcon, ListItemText, SvgIcon } from '@mui/material';
 import { ArrowDropDown } from '@mui/icons-material';
 import DesktopWindowsIcon from "@mui/icons-material/DesktopWindows"
 import { ReactComponent as Termi } from "../assets/TerminalIcon.svg";
+
 const DrawerListing = () => {
+
+    const [open1, setOpen1] = useState(false);
+
+    const handleClick = (num) => {
+        if (num === "1") setOpen1(!open1);
+    }
+
     return (
         <div>
             <Box>
                 <List>
-                    <ListItemButton>
+                    {/* System */}
+                    <ListItemButton onClick={() => handleClick('1')}>
                         <ListItemIcon>
-                            {/* System */}
                             <DesktopWindowsIcon />
                         </ListItemIcon>
                         <ListItemText primary="System" />
                         <ArrowDropDown />
                     </ListItemButton>
-                    <Collapse in={true} timeout="auto" unmountOnExit>
-                        <ListItemButton sx={{ pl: 5 }}>
-                            <ListItemIcon>
-                                <SvgIcon component={Termi} inheritViewBox></SvgIcon>
-                            </ListItemIcon>
-                            <ListItemText primary="Terminal" />
-                        </ListItemButton>
-                    </Collapse>
+                    <SystemCollapse isOn={open1} />
+
+            
                 </List>
             </Box>
         </div>
     )
-}
+};
 
 export default DrawerListing;
+
+
+const SystemCollapse = (props) => {
+    return (
+        <Collapse in={props.isOn} timeout="auto" unmountOnExit>
+            <ListItemButton sx={{ pl: 5 }}>
+                <ListItemIcon>
+                    <SvgIcon component={Termi} inheritViewBox></SvgIcon>
+                </ListItemIcon>
+                <ListItemText primary="Terminal" />
+            </ListItemButton>
+        </Collapse>
+    );
+};
