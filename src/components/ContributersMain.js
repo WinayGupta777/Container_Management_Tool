f6110b1ab4d0374e0f9660f41c9bf66b315f1c03
WinayGupta777 <vinay.gupta8101@gmail.com>
2022-08-13 18:46:27 +0530

    Changed Avatar to Names; Looks good now;

--- src/components/ContributersMain.js
+++ src/components/ContributersMain.js
@@ -1,29 +1,37 @@
 import React from 'react'
-import { Avatar, Box, AvatarGroup, Tooltip } from '@mui/material';
-import f1 from "../static/Images/f1.jpg";
-import m1 from "../static/Images/m1.jpg";
-import m2 from "../static/Images/m2.jpg";
-import m3 from "../static/Images/m3.jpg";
+import { Box, Typography } from '@mui/material';
 
 const ContributersMain = () => {
     return (
-        <Box sx={{ p: 2, display: 'flex', alignItems: 'center', justifyContent: 'center' }} >
-            <AvatarGroup max={4} >
-                <Tooltip title="Dolly">
-                    <Avatar src={f1} sx={{ width: 55, height: 55 }} />
-                </Tooltip>
-                <Tooltip title="Trevor Henderson">
-                    <Avatar src={m3} sx={{ width: 55, height: 55 }} />
-                </Tooltip>
-                <Tooltip title="Agnes Walker">
-                    <Avatar src={m2} sx={{ width: 55, height: 55 }} />
-                </Tooltip>
-                <Tooltip title="Travis Howard">
-                    <Avatar src={m1} sx={{ width: 55, height: 55 }} />
-                </Tooltip>
-            </AvatarGroup>
+        <Box sx={{ p: 1, display: 'flex', alignItems: 'center', justifyContent: 'space-between', flexDirection: 'column' }} >
+            <Typography sx={{ mb: 2 }}>Developed by</Typography>
+            <Box
+                sx={{
+                    display: 'grid', gridColumnGap: 10,
+                    gridAutoFlow: 'column dense',
+                    gridTemplateRows: 'repeat(2, 45px [row-start])',
+                    gridTemplateColumns: 'repeat(2)'
+                }}
+            >
+                <ContributerBox name="Dolly" />
+                <ContributerBox name="Tanjeem" />
+                <ContributerBox name="Aakriti" />
+                <ContributerBox name="Harshit" />
+                <ContributerBox name="Ramya" />
+                <ContributerBox name="+1" />
+
+            </Box>
         </Box>
     )
 }
 
 export default ContributersMain;
+
+const ContributerBox = (props) => {
+    return (
+        <Box sx={{
+            justifySelf: 'center', p: 1, alignSelf: 'center',width: 70, backgroundColor: (theme) => theme.palette.mode === 'dark' ? '#272727' : '#e6ffec',
+            border: '1px solid none', borderRadius: 1, fontSize: 14, textAlign: 'center',
+        }}>{props.name}</Box>
+    )
+};
