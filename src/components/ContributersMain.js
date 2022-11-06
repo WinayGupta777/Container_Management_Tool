c334116bf87ec3fb690d4ff6b6f54420ba276d0e
WinayGupta777 <vinay.gupta8101@gmail.com>
2022-11-06 10:27:37 +0530

    Modified Contributers plate;

--- src/components/ContributersMain.js
+++ src/components/ContributersMain.js
@@ -4,22 +4,16 @@ import { Box, Typography } from '@mui/material';
 const ContributersMain = () => {
     return (
         <Box sx={{ p: 1, display: 'flex', alignItems: 'center', justifyContent: 'space-between', flexDirection: 'column' }} >
-            <Typography sx={{ mb: 2 }}>Developed by</Typography>
+            <Typography sx={{ mb: 2 }}>Developer</Typography>
             <Box
                 sx={{
-                    display: 'grid', gridColumnGap: 10,
-                    gridAutoFlow: 'column dense',
-                    gridTemplateRows: 'repeat(2, 45px [row-start])',
-                    gridTemplateColumns: 'repeat(2)'
+                    display: 'grid', gridColumnGap: 10, mb: 2,
+                    // gridAutoFlow: 'column dense',
+                    // gridTemplateRows: 'repeat(2, 45px [row-start])',
+                    // gridTemplateColumns: 'repeat(2)'
                 }}
             >
-                <ContributerBox name="Dolly" />
-                <ContributerBox name="Tanjeem" />
-                <ContributerBox name="Aakriti" />
-                <ContributerBox name="Harshit" />
-                <ContributerBox name="Ramya" />
-                <ContributerBox name="+1" />
-
+                <ContributerBox name="Vinay Gupta" />
             </Box>
         </Box>
     )
@@ -30,7 +24,7 @@ export default ContributersMain;
 const ContributerBox = (props) => {
     return (
         <Box sx={{
-            justifySelf: 'center', p: 1, alignSelf: 'center',width: 70, backgroundColor: (theme) => theme.palette.mode === 'dark' ? '#272727' : '#e6ffec',
+            justifySelf: 'center', p: 1, alignSelf: 'center',width: 100, backgroundColor: (theme) => theme.palette.mode === 'dark' ? '#272727' : '#e6ffec',
             border: '1px solid none', borderRadius: 1, fontSize: 14, textAlign: 'center',
         }}>{props.name}</Box>
     )
