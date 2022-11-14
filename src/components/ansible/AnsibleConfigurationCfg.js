118d14501cec9e926a6c6b1c022a9a998742e6ae
WinayGupta777 <vinay.gupta8101@gmail.com>
2022-11-14 11:08:10 +0530

    Applied Editor to both component; Theme for editor applied;

--- src/components/ansible/AnsibleConfigurationCfg.js
+++ src/components/ansible/AnsibleConfigurationCfg.js
@@ -1,16 +1,29 @@
-import { Box, Divider, Typography } from '@mui/material';
 import React from 'react'
+import { Box, Divider, Typography } from '@mui/material';
+import CodeMirror from "@uiw/react-codemirror";
+import { dracula } from '@uiw/codemirror-theme-dracula';
+import { bbedit } from '@uiw/codemirror-theme-bbedit';
+//import { loadLanguage } from '@uiw/codemirror-extensions-langs';
+import { useSelector } from 'react-redux';
 import "../../css/AnsibleConfigureMain.css";
 
 const AnsibleConfigurationCfg = () => {
+  const globeTheme = useSelector(
+    (state) => state.themeReducer.theme
+  );
   return (
-    <Box
-      className='cfg'
-      sx={{ backgroundColor: (theme) => theme.palette.mode == 'dark' ? 'rgb(36, 36, 36)' : '#e6ffec' }}
-    >
+    <Box className='cfg'>
       <Typography sx={{ p: 1, fontFamily: 'consolas', fontWeight: '600' }}>ansible.cfg</Typography>
       <Divider />
 
+      <CodeMirror 
+        width='auto'
+        height='82vh'
+        //value={code}
+        theme={globeTheme ? dracula : bbedit}
+        //onChange={(editor)=>onCodeChange(editor)}
+        style={{fontSize: 20}}
+      />
     </Box>
   )
 }
--- src/components/ansible/AnsibleConfigurationInv.js
+++ src/components/ansible/AnsibleConfigurationInv.js
@@ -1,16 +1,29 @@
 import { Box, Divider, Typography } from '@mui/material';
 import React from 'react'
+import { useSelector } from 'react-redux';
+import CodeMirror from "@uiw/react-codemirror";
+import { dracula } from '@uiw/codemirror-theme-dracula';
+import { bbedit } from '@uiw/codemirror-theme-bbedit';
+//import { loadLanguage } from '@uiw/codemirror-extensions-langs';
 import "../../css/AnsibleConfigureMain.css";
 
 const AnsibleConfigurationInv = () => {
+  const globeTheme = useSelector(
+    (state) => state.themeReducer.theme
+  );
   return (
-    <Box
-      className='inv'
-      sx={{ backgroundColor: (theme) => theme.palette.mode == 'dark' ? 'rgb(36, 36, 36)' : '#e6ffec' }}
-    >
+    <Box className='inv'>
       <Typography sx={{ p: 1, fontFamily: 'consolas', fontWeight: '600' }}>inventory</Typography>
       <Divider />
       
+      <CodeMirror 
+        width='auto'
+        height='82vh'
+        //value={code}
+        theme={globeTheme ? dracula : bbedit}
+        //onChange={(editor)=>onCodeChange(editor)}
+        style={{fontSize: 20}}
+      />
     </Box>
   )
 }
